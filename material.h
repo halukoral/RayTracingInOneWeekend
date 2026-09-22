1e9371d6e796b35061193bdb40e8dbf4d94fd2bc
Haluk Oral <halukoral0@gmail.com>
2026-09-23 01:15:21 +0300

    materials created

---
+++ material.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "hittable.h"
+
+class material 
+{
+  public:
+    virtual ~material() = default;
+
+    virtual bool scatter(const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered) const 
+    {
+        return false;
+    }
+};
