fe62945c86ab823816b73212c6064e499e1439ce
Haluk Oral <halukoral0@gmail.com>
2026-09-22 19:20:31 +0300

    Sphere Coloring Based on Normal Vectors

--- main.cpp
+++ main.cpp
@@ -4,20 +4,33 @@
 #include "vec3.h"
 #include "ray.h"
 
-bool hit_sphere(const point3& center, double radius, const ray& r) 
+double hit_sphere(const point3& center, double radius, const ray& r) 
 {
 	vec3 oc = center - r.origin();
 	auto a = dot(r.direction(), r.direction());
 	auto b = -2.0 * dot(r.direction(), oc);
 	auto c = dot(oc, oc) - radius * radius;
 	auto discriminant = b * b - 4 * a * c;
-	return (discriminant >= 0);
+	
+	if (discriminant < 0) 
+	{
+		return -1.0;
+	} 
+	else 
+	{
+		return (-b - std::sqrt(discriminant) ) / (2.0*a);
+	}
+
 }
 
 color ray_color(const ray& r) 
 {
-	if (hit_sphere(point3(0, 0, -1), 0.5, r))
-		return color(1, 0, 0);
+	auto t = hit_sphere(point3(0,0,-1), 0.5, r);
+	if (t > 0.0) 
+	{
+		vec3 N = unit_vector(r.at(t) - vec3(0,0,-1));
+		return 0.5*color(N.x()+1, N.y()+1, N.z()+1);
+	}
 
 	vec3 unit_direction = unit_vector(r.direction());
 	auto a = 0.5 * (unit_direction.y() + 1.0);
@@ -28,7 +41,7 @@ int main()
 {
 	// Image
 	auto aspect_ratio = 16.0 / 9.0;
-	int image_width = 400;
+	int image_width = 800;
 	
 	// Calculate the image height, and ensure that it's at least 1.
 	int image_height = int(image_width / aspect_ratio);
